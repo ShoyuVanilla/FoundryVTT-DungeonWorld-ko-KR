039688c8fc0b9fd73d9b32af70a40d49e62ea8df
Kyuho Lee <modulo641@gmail.com>
2020-05-21 19:40:20 +0900

    Modify tag_translator.py

--- compendium/tag_translator.py
+++ compendium/tag_translator.py
@@ -3,6 +3,10 @@ import collections
 import sys
 import os
 
+# replace_path = 'replaces.json'
+# with open(replace_path) as f:
+# 	replace_data = json.load(f)['entries']
+
 def replace_tags_in_string(str, tags):
 	swap0 = str
 	swap1 = str
@@ -23,7 +27,13 @@ def translate_tags(path, tags):
 	for entry in data['entries']:
 		if 'tags' in entry:
 			entry['tags'] = replace_tags_in_string(entry['tags'], tags)
-
+		if '|' in entry['id']:
+			splits = entry['id'].split('|', 1)
+			entry['id'] = splits[0]
+			entry['name'] = splits[1]
+		# for replace_entry in replace_data:
+		# 	if entry['name'] == replace_entry['name']:
+		# 		entry['description'] = replace_entry['description']
 	with open(path, "w") as f:
 		json.dump(data, f, ensure_ascii=False, indent=2)
 
@@ -33,5 +43,5 @@ with open(tag_path) as f:
 	tags = json.load(f)['entries']
 
 for path in os.listdir('./'):
-	if path.endswith(".json") and tag_path not in path:
+	if path.endswith(".json") and tag_path not in path: #and replace_path not in path:
 		translate_tags(path, tags)
