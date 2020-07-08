4549646f572574d3d8380938e1704e6f20346692
Kyuho Lee <modulo641@gmail.com>
2020-07-09 04:02:05 +0900

    Various changes

--- babele-register.js
+++ babele-register.js
@@ -1,10 +1,9 @@
 Hooks.on('init', () => {
-
 	if(typeof Babele !== 'undefined') {
 		Babele.get().register({
 			module: 'DungeonWorld-ko',
 			lang: 'ko',
 			dir: 'compendium'
-		}); 
+		});
 	}
 });
